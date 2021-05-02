0d1d0efc9b28677030bf7b036a08ec97a6264052
kadriozcan <kadriozcan38@gmail.com>
2021-05-02 17:11:55 +0300

    miniProjeSesliHarfler

---
+++ miniProje2SesliHarfler/src/miniProje2SesliHarfler/Main.java
@@ -0,0 +1,23 @@
+package miniProje2SesliHarfler;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		char harf = 'i';
+
+		switch (harf) {
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("kalin");
+			break;
+
+		default:
+			System.out.println("ince");
+		}
+
+	}
+
+}
